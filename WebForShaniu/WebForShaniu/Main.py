5108a1de91418c51832e4e90f7e022a21b9069fa
WindowsShen <310059703@qq.com>
2017-08-13 18:16:25 +0800

    Add if password error's handle

--- WebForShaniu/WebForShaniu/Main.py
+++ WebForShaniu/WebForShaniu/Main.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def CheckPassword(request):
     if request.POST and 'woaidaguanjia' == request.POST['Password'].lower():
         return render(request, 'Second.html')
+    else:
+        return render(request, 'Main.html')
